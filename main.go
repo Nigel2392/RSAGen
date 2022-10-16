c0927a6484c954b050b692fdc1ef520d1472e71f
Nigel van Keulen <Nigelvk@itextra.nl>
2022-10-16 20:16:43 +0200

    Added more flags

--- main.go
+++ main.go
@@ -81,7 +81,11 @@ Usage:
 ` + color.Colorize(color.Blue, "  RSAGen.exe -generate") + `
   	Generate a new keypair
 ` + color.Colorize(color.Blue, "  RSAGen.exe -privkey string") + `
-  	Private key file`
+  	Private key file
+` + color.Colorize(color.Blue, "  RSAGen.exe -privkey string") + `
+	Private key file name (default "PRIVKEY.pem")
+` + color.Colorize(color.Blue, "  RSAGen.exe -privkey string") + `
+	Public key file name (default PUBKEY.pem)`
 	fmt.Println(color.Colorize(color.Purple, str))
 }
 
@@ -92,13 +96,23 @@ func PrintExtraInfo() {
 
 }
 
+func PrintErr(err error) {
+	if err != nil {
+		fmt.Println(color.Colorize(color.Red, "An error has ocurred: "+err.Error()))
+		panic(err)
+	}
+}
+
 func main() {
 	if len(os.Args) == 1 {
 		PrintInfo()
+		fmt.Println(color.Colorize(color.Red, "Error: No arguments provided"))
 		return
 	}
 
 	generate := flag.Bool("generate", false, "Generate a new keypair")
+	privkey_fname := flag.String("privname", "PRIVKEY.pem", "Private key file name (default: PRIVKEY.pem)")
+	pubkey_fname := flag.String("pubname", "PUBKEY.pem", "Public key file name (default: PUBKEY.pem)")
 	bits := flag.Int("bits", 2048, "Number of bits for the keypair")
 	privkeyfile := flag.String("privkey", "", "Private key file")
 	extra_info := flag.Bool("info", false, "Extra info")
@@ -107,18 +121,19 @@ func main() {
 
 	if *generate {
 		privkey, pubkey, err := GenKeyPair(*bits)
-		if err != nil {
-			panic(err)
-		}
-		ExpPriv_PEM(privkey, "PRIVKEY.pem")
-		ExpPub_PEM(pubkey, "PUBKEY.pem")
+		PrintErr(err)
+		err = ExpPriv_PEM(privkey, *privkey_fname)
+		PrintErr(err)
+		err = ExpPub_PEM(pubkey, *pubkey_fname)
+		PrintErr(err)
 	}
 	if *privkeyfile != "" {
 		if *generate {
 			fmt.Println(color.Colorize(color.Red, "You can't generate a keypair and import a private key at the same time"))
 			return
 		}
-		WritePubFromPriv(*privkeyfile)
+		err := WritePubFromPriv(*privkeyfile)
+		PrintErr(err)
 	}
 	if *extra_info {
 		PrintInfo()
